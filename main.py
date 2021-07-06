450610fc76777a51eb385c4eb647213fcb8ce4af
Kostia Mazurkevich <mazurkevichkostia@gmail.com>
2021-07-06 18:05:08 +0300

    Implement unique task

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
-print("Hello, World")
+from random import randint
+
+src = [randint(-100, 100) for _ in range(30)]
+print(f'Source list: {src}')
+maxint = max(src)
+print('Max item: {} by the index of {}'.format(maxint, src.index(maxint)))
+odds = sorted([num for num in src if num % 2], reverse=True)
+if not odds:
+    print('Odd numbers not found')
+else:
+    print(odds)
+
